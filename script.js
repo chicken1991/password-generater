a0e050c94fbcb6d8b4eb5aa7f8ef0e60be305ed0
benpitroff <benpitroff@gmail.com>
2022-04-18 18:53:36 -0700

    start of data validation branch

--- script.js
+++ script.js
@@ -29,40 +29,90 @@ function generatePassword() {
   // array to select random chars
   var superArray = [];
 
+  var lowerCaseChars = "abcsefghijklmnopqrstuvwxyz"
+  var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numericChars = "0123456789"
+  var specialChars = "!\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"
+  var pwLength;
+
   //This object "Inputs" prompts the user for inputs, and stores the chars
-  var inputs = {
-   lowercase: {
-      userPrompt: prompt("Use lowercase letters?\ny or n"),
-      chars: "abcsefghijklmnopqrstuvwxyz"
-    },
-    uppercase: { 
-      userPrompt: prompt("Use uppercase letters?\ny or n"),
-      chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    },
-    numeric: {
-      userPrompt: prompt("Use numeric characters?\ny or n"),
-      chars: "0123456789"
-    },
-    special: {
-      userPrompt: prompt("Use special characters?\ny or n"),
-      chars: "!\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"
-    },
-    pwLength: {
-      userPrompt: prompt("How many characters in this password?\nBetween 8 and 128"),
-      chars: ""
-    },
-  }
+  // var inputs = {
+  //  lowercase: {
+  //     userPrompt: prompt("Use lowercase letters?\ny or n", "y"),
+  //     chars: "abcsefghijklmnopqrstuvwxyz"
+  //   },
+  //   uppercase: { 
+  //     userPrompt: prompt("Use uppercase letters?\ny or n", "y"),
+  //     chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  //   },
+  //   numeric: {
+  //     userPrompt: prompt("Use numeric characters?\ny or n", "y"),
+  //     chars: "0123456789"
+  //   },
+  //   special: {
+  //     userPrompt: prompt("Use special characters?\ny or n", "y"),
+  //     chars: "!\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"
+  //   }
+  // }
+
 
   // This string will be a combination of the "True" isEnabled chars
   var bigString = "";
 
+  //Prompt window for user inputs. Utilize if-else and while statments for data validation
+  function prompter(){
+    var pwLength = prompt("How many characters in this password?\nBetween 8 and 128", "15");
+  if (pwLength <= 7 || pwLength > 128){
+    pwLength = prompt("ERROR: Please input a number between 8 and 128", "15");
+  }
+
+  // ===========lowerAnswer================
+  
+  //This do-while loop will continue to prompt until an answer is given.
+  do{
+    lowerAnswer = prompt("Use lowercase letters?\ny or n", "y");
+    lowerAnswer = lowerAnswer.toLowerCase();
+    if(lowerAnswer !== "y" || "n"){
+      loweranswer = "what the fuck";
+    }
+  } while (!lowerAnswer);
+
+  if (lowerAnswer === "y") {
+    bigString += lowerCaseChars;
+  } else if (lowerAnswer === "n"){
+
+  }
+  
+  // lowerAnswer = prompt("Use lowercase letters?\ny or n", "y");
+  // lowerAnswer = lowerAnswer.toLowerCase();
+
+  // while(lowerAnswer !== "y" || "n"){
+  //   lowerAnswer = prompt("Woah there buddy. Please respond with either 'y' or 'n'.");
+  // }
+  // lowerAnswer = lowerAnswer.toLowerCase();
+  // console.log(lowerAnswer);
+  // while(!lowerAnswer){
+  //   if (lowerAnswer === "y") {
+  //     bigString += lowerCaseChars;
+  //   } else if (lowercase === "n"){
+
+  //   } else {
+  //       lowerAnswer = "";
+  //   }
+  // } 
+
+  console.log(lowerAnswer);
+
+
+}
+
   // Iterate through all object properties, determine the answer, flag it for isEnabled
   //  and add the chars to the bigString
-    for (var x in inputs){
-      if(inputs[x].userPrompt !== "n") {
-        bigString += inputs[x].chars;
-      }
-    }
+    // for (var x in inputs){
+    //   if(inputs[x].userPrompt !== "n") {
+    //     bigString += inputs[x].chars;
+    //   }
+    // }
 
     //Create superArray from the combinated bigString
     var superArray = bigString.split("");
@@ -72,11 +122,12 @@ function generatePassword() {
   // check out the Fisher Yates method
   // Pull random characters from superArray and put them together as a string
   function randomizer(){
-    for (var i = 0; i < inputs.pwLength.userPrompt; i++){
+    for (var i = 0; i < pwLength; i++){
       newPass += superArray[Math.floor(Math.random() * superArray.length)];
     }
   }
 
+  prompter();
   randomizer();
   return newPass;
 
